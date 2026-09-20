50964d4b266feb8b6acdef9b7e42f06e5645e884
alewnardu <lewnardu@gmail.com>
2026-09-20 02:52:14 -0300

    Implementa arquivo de consulta de couses

---
+++ src/Courses/findMany.js
@@ -0,0 +1,26 @@
+import "dotenv/config";
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({
+    adapter,
+});
+
+async function main() {
+    const result = await prisma.course.findMany();
+
+    console.log(result);
+}
+
+main()
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
