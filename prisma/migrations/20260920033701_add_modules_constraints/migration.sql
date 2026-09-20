82f5f07efba189e925a2c0963b9bf29131b27c46
alewnardu <lewnardu@gmail.com>
2026-09-20 02:21:17 -0300

    Corrige constraints para os campos update_at

--- prisma/migrations/20260920033701_add_modules_constraints/migration.sql
+++ prisma/migrations/20260920033701_add_modules_constraints/migration.sql
@@ -1,4 +1,4 @@
 -- This is an empty migration.
-ALTER TABLE modules ADD CONSTRAINT chk_update_at CHECK (updated_at > created_at);   
+ALTER TABLE modules ADD CONSTRAINT chk_update_at CHECK (updated_at >= created_at);   
 
 ALTER TABLE modules ADD CONSTRAINT chk_nome CHECK (length(name) > 3);
