da7122cf868ef92e0d1f4592be496640c7735d3f
alewnardu <lewnardu@gmail.com>
2026-09-21 18:50:30 -0300

    Remove migrações e scripts relacionado ao domínio da aula anterior.

---
+++ prisma/migrations/20260921204937_create_table_books/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "books" (
+    "id" UUID NOT NULL,
+    "name" VARCHAR(150) NOT NULL,
+    "author_id" UUID NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "books_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "books" ADD CONSTRAINT "books_author_id_fkey" FOREIGN KEY ("author_id") REFERENCES "authors"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
